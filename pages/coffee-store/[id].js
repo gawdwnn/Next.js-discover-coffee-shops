95b4dc241694cf5eb4e33ee7143d3558624c9d55
godwin <smarttin77@gmail.com>
2022-02-05 17:36:44 +0100

    setup, styling

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,13 @@
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+
+const CoffeeStore = () => {
+  const router = useRouter()
+  
+  return <div></div>;
+};
+
+export default CoffeeStore;
